1d1918cf547e4dcd8709b2a13206f3a1242ec497
Monineath Sar <monineathsar@gmail.com>
2022-05-30 19:44:47 -0700

    working JS and fixed some CSS

--- Assets/script.js
+++ Assets/script.js
@@ -42,6 +42,71 @@ const businessHour = [
     }
 ];
 
+
+
+//creates timeblock div elements inside container div
+document.addEventListener('DOMContentLoaded', function() {
+    var timeBlocksDiv = document.getElementById("timeBlocks");
+    businessHour.forEach(function(x){
+        var outerDiv = document.createElement('div');
+        outerDiv.id = x.id;
+        outerDiv.classList.add("time-block");
+        outerDiv.classList.add("row");
+        
+        var innerDiv = document.createElement('div');
+        innerDiv.innerHTML = x.txt
+        innerDiv.classList.add("hour");
+        outerDiv.appendChild(innerDiv);
+
+        var textArea = document.createElement("textarea");
+        textArea.classList.add("description");
+        outerDiv.appendChild(textArea);
+
+        var button = document.createElement('button');
+        var img = document.createElement("img");
+        img.src = "./Assets/saveIcon.png"
+        button.appendChild(img);
+        button.classList.add("saveBtn");
+
+        outerDiv.appendChild(button);
+
+        timeBlocksDiv.appendChild(outerDiv);
+    });
+
+}, false);
+
 // formats the date at top of page
 var todayDate = moment().format('dddd, MMMM Do YYYY, h:mm a');
 $("#currentDay").html(todayDate);
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var id = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();
+        localStorage.setItem(id, text);
+    })
+
+    function timeTracker() {
+        var currentHour = moment().hour();
+
+        $(".time-block").each(function() {
+            var currentTimeblock = parseInt($(this).attr("id"));
+
+            if (currentTimeblock < currentHour) {
+                $(this).addClass("past");
+            }
+            else if (currentTimeblock === currentHour) {
+                $(this).addClass("present");
+            }
+            else {
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    businessHour.forEach(function(x) {
+        $("#" + x.id + " .description").val(localStorage.getItem(x.id));
+    });
+
+    timeTracker();
+})
